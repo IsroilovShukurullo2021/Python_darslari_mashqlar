9f2d469d2042453924edfa8e3486a95af0a7531a
IsroilovShukurullo2021 <ishbuzcoders@gmail.com>
2021-06-30 12:17:48 +0300

    first commit

---
+++ Python_mashqlari.py
@@ -0,0 +1,8 @@
+qs=int(input('son kiriting: '))
+for i in range(1,20):
+    if qs==i:
+        print('bor')
+        break
+    else:
+        #print('yo`q')
+        continue
---
+++ main.py
@@ -0,0 +1,8 @@
+ism='Shukurullo'
+for i in ism:
+    print(i)
+
+k=0
+while k<len(ism):
+    print(ism[k])
+    k+=1
